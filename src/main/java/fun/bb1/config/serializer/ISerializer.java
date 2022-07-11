481d453c4ae0a54eb5b4653cd84260923be66be5
BradBot_1 <45739045+BradBot1@users.noreply.github.com>
2022-07-11 22:16:51 +0100

    Bump ver + add method to convert a serializer to take objects
    This does utilise casting

--- src/main/java/fun/bb1/config/serializer/ISerializer.java
+++ src/main/java/fun/bb1/config/serializer/ISerializer.java
@@ -2,6 +2,7 @@ package fun.bb1.config.serializer;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.ApiStatus.Internal;
 
 import fun.bb1.objects.Primitive;
 import fun.bb1.registry.IRegisterable;
@@ -29,7 +30,7 @@ import fun.bb1.registry.IRegisterable;
  * 
  * @author BradBot_1
  */
-public interface ISerializer<T> extends IRegisterable<Class<? extends T>> {
+public interface ISerializer<T extends Object> extends IRegisterable<Class<? extends T>> {
 	/**
 	 * Deserializes the provided {@link Primitive} into T
 	 * 
@@ -54,6 +55,24 @@ public interface ISerializer<T> extends IRegisterable<Class<? extends T>> {
 	@Override
 	default void register(@NotNull final Class<? extends T> arg0) {
 		SerializerController.getSerializerRegistry().register(arg0, this);
+	}
+	
+	@Internal
+	public default @NotNull ISerializer<Object> toObjectSerializer() {
+		final ISerializer<T> self = this;
+		return new ISerializer<Object>() {
+
+			@Override
+			public @Nullable Object deserialize(@NotNull Primitive primitiveForm) {
+				return self.deserialize(primitiveForm);
+			}
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public @Nullable Primitive serialize(@NotNull Object instanceOfT) {
+				return self.serialize((T)instanceOfT);
+			}
+		};
 	}
 	
 }
